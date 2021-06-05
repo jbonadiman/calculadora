b509346980b67ebb6db64f183eaeef1516126cdb
João Vitor Bonadiman <j.bonadiman@live.com>
2021-06-05 17:50:42 -0300

    Adiciona números fracionários;

--- js/operation.js
+++ js/operation.js
@@ -4,30 +4,78 @@ class Operation {
   ]
 
   static MAX_DIGITS = 9;
-  static INTL = Intl.NumberFormat();
+  static INTL = Intl.NumberFormat(undefined, {
+    maximumFractionDigits: 8
+  });
+  
+  static DECIMAL_SEP = null;
+  static THOUSAND_SEP = null;
 
   constructor() {
     this.operands = [];
     this.operator = null;
     this.currentOperand = '0'
     this.value = 0;
+
+    if (!Operation.DECIMAL_SEP || !Operation.THOUSAND_SEP) {
+      const parts =
+        Intl.NumberFormat()
+          .formatToParts(1000.1);
+
+      Operation.DECIMAL_SEP = parts
+        .find(part => part.type === 'decimal')
+        .value;
+
+      Operation.THOUSAND_SEP = parts
+        .find(part => part.type === 'group')
+        .value;
+    }
   }
 
   addDigit(stringDigit) {
-    if (this.currentOperand.length >= Operation.MAX_DIGITS) {
+    if (this.currentOperand.match(/\d+/g).length >= Operation.MAX_DIGITS) {
       alert(`Não é possível inserir mais de ${Operation.MAX_DIGITS} dígitos`);
       return;
     }
 
-    if (Number(this.currentOperand) === 0) {
+    if (this.currentOperand.endsWith('.0')) {
+      this.currentOperand = this.currentOperand.replace('.0', `.${stringDigit.trim()}`);
+    } else if (Number(this.currentOperand) === 0) {
       this.currentOperand = stringDigit.trim();
     } else {
       this.currentOperand += stringDigit.trim();
     }
   }
 
+  addDecimal(separator) {
+    if (separator === Operation.DECIMAL_SEP) {
+      console.debug(`decimal separator: ${separator}`)
+
+      if (this.currentOperand.match(/\d+/g).length >= Operation.MAX_DIGITS) {
+        alert(`Não é possível inserir mais de ${Operation.MAX_DIGITS} dígitos`);
+        return;
+      }
+
+      this.currentOperand += '.0';
+    } else {
+      console.debug(`thousand separator: ${separator}`)
+    }
+  }
+
+  deleteDigit() {
+    if (this.currentOperand.length === 1) {
+      this.currentOperand = '0';
+    }
+
+    this.currentOperand = this.currentOperand.substring(0, this.currentOperand.length - 1);
+  }
+
   getCurrentOperand() {
-    return Operation.INTL.format(Number(this.currentOperand));
+    let formattedOperand = Operation.INTL.format(Number(this.currentOperand));
+
+    console.debug({formattedOperand});
+
+    return formattedOperand;
   }
 }
 
--- js/script.js
+++ js/script.js
@@ -1,18 +1,47 @@
 import Display from './display.js';
 import Operation from './operation.js';
 
-let display = new Display();
-let operation = new Operation();
+const display = new Display();
+const operation = new Operation();
+const recognizedKeys = '[0-9,.]|Backspace';
 
 display.update(operation.getCurrentOperand());
 
-if (document.addEventListener) {
-  document.addEventListener('keyup', (evt) => {
-    let {key} = evt;
 
-    if (key >= '0' && key <= '9') {
-      operation.addDigit(key);
+let digits = [...document.getElementsByClassName('digit')];
+digits.forEach(btn => {
+  if (btn.addEventListener) {
+    btn.addEventListener('click', () => {
+      operation.addDigit(btn.textContent);
+      display.update(operation.getCurrentOperand());
+    })
+  } else {
+    console.error('Browser not compatible!');
+  }
+});
+
+
+if (document.addEventListener) {
+  document.addEventListener('keyup', evt => {
+    let {key} = evt;
+    console.debug({key})
+
+    if (key.match(new RegExp(recognizedKeys, "g"))) {
+      if (key.match(/\d/)) {
+        operation.addDigit(key);
+      }
+
+      if (key.match(/[.,]/g)) {
+        operation.addDecimal(key);
+      }
+
+      if (key === 'Backspace') {
+        operation.deleteDigit();
+      }
+
       display.update(operation.getCurrentOperand());
     }
   }, false);
+} else {
+  console.error('Browser not compatible!');
 }
