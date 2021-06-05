299f9f9771a6eecb70b8e13423dd3b1c12625604
João Vitor Bonadiman <j.bonadiman@live.com>
2021-06-05 18:04:37 -0300

    Refatora ações de "dígitos" para que os botões também funcionem;

--- js/script.js
+++ js/script.js
@@ -3,37 +3,45 @@ import Operation from './operation.js';
 
 const display = new Display();
 const operation = new Operation();
-const recognizedKeys = '[0-9,.]|Backspace';
+const recognizedKeys = '^[0-9,.]$|^Backspace$';
 
 display.update(operation.getCurrentOperand());
 
+function doDigitAction(input) {
+  if (input.match(/^\d$/)) {
+    operation.addDigit(input);
+  }
+
+  if (input.match(/^[.,]$/)) {
+    operation.addDecimal(input);
+  }
+}
 
 let digits = [...document.getElementsByClassName('digit')];
-digits.forEach(btn => {
-  if (btn.addEventListener) {
-    btn.addEventListener('click', () => {
-      operation.addDigit(btn.textContent);
-      display.update(operation.getCurrentOperand());
-    })
-  } else {
-    console.error('Browser not compatible!');
-  }
-});
 
+digits
+  .filter(dg => dg.id === 'dot')
+  .forEach(dg => dg.textContent = Operation.DECIMAL_SEP);
+
+digits
+  .forEach(btn => {
+    if (btn.addEventListener) {
+      btn.addEventListener('click', () => {
+        doDigitAction(btn.textContent);
+        display.update(operation.getCurrentOperand());
+      })
+    } else {
+      console.error('Browser not compatible!');
+    }
+  });
 
 if (document.addEventListener) {
   document.addEventListener('keyup', evt => {
     let {key} = evt;
     console.debug({key})
 
-    if (key.match(new RegExp(recognizedKeys, "g"))) {
-      if (key.match(/\d/)) {
-        operation.addDigit(key);
-      }
-
-      if (key.match(/[.,]/g)) {
-        operation.addDecimal(key);
-      }
+    if (key.match(new RegExp(recognizedKeys))) {
+      doDigitAction(key);
 
       if (key === 'Backspace') {
         operation.deleteDigit();
